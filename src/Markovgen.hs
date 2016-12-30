8bbdb74ac5f7b136cdcb96d3c39de7604510aa0c
Anthony Liu <liu916@gmail.com>
2016-12-30 14:49:19 -0500

    add work

--- src/Markovgen.hs
+++ src/Markovgen.hs
@@ -1,21 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Markovgen
-    ( MChain
+    ( Nodes
+    , Edges
     , makeMChain
     , someFunc
     ) where
 
 import qualified Data.Text as T
 import qualified Data.Map as Map
+import Control.Arrow ((***))
+import Data.Sequence as Seq
 import Data.List (foldl')
+import Data.Function (on)
 import Data.Random
 
-newtype MChain a = MChain { unMChain :: Map.Map [a] Int }
+newtype Edges a = Edges {unEdges :: Map.Map a Int}
 
-makeMChain :: Int -> [a] -> MChain a
-makeMChain n = MChain . foldl' go Map.empty
+newtype Nodes a = Nodes { unNodes :: Map.Map (Seq.Seq a) (Edges a) }
+
+makeMChain :: (Ord a) => Int -> a -> [a] -> Nodes a
+makeMChain n term = Nodes . lastchain . foldl' go (Map.empty, Seq.replicate n term)
   where
+    combineEdges = ((.) . (.)) Edges $ on (Map.unionWith (+)) unEdges
+    updateNodes chain n = 
+      Nodes . Map.insertWith combineEdges chain (Edges $ Map.singleton n 1) . unNodes
+    lastchain (mp, last) = mp
     go = const
 
 someFunc :: IO ()
